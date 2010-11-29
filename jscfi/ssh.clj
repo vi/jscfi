6081dffe843fd720ba8e1d3629875fee980e18ea
Vitaly _Vi Shukela <public_vi@tut.by>
2010-11-29 19:52:38 +0200

    Enhance status area reporting, minor refactor

--- jscfi/ssh.clj
+++ jscfi/ssh.clj
@@ -3,8 +3,9 @@
   (:import (java.io.ByteArrayInputStream))
   )                
 
+(def ssh-session (agent nil))
 
-(defn get-ssh-session [status-observer server port user password] 
+(defn login-to-server [sess status-observer server port user password]
     (status-observer "initializing connection")
     (def jsch (JSch.))
     (def session (.getSession jsch user server port))
@@ -19,37 +20,42 @@
     (status-observer "connecting")
     (.connect session 3000)
     (status-observer "connected")
-    session
+    {
+	:session,session
+	:status-observer,status-observer
+    }
 )
 
-(def ssh-session (agent nil))
-
-(defn login-to-server [sess status-observer server port user password]
-    (def session (if (nil? sess) (get-ssh-session status-observer server port user password) sess))
-    session
-)
-
-(defn upload-file-to-server [session file]
-    (def sftp (.openChannel session "sftp"))
+(defn upload-file-to-server [sess file]
+    ((get sess :status-observer) "initializing upload")
+    (def sftp (.openChannel (get sess :session) "sftp"))
     (.connect sftp 3000)
+    ((get sess :status-observer) "uploading")
     (.put sftp file "source.c" ChannelSftp/OVERWRITE)
+    ((get sess :status-observer) "uploded")
     (.disconnect sftp)
 
-    (def shell (.openChannel session "shell"))
+    ((get sess :status-observer) "initializing complilation")
+    (def shell (.openChannel (get sess :session) "shell"))
     (.setOutputStream shell System/out)
     (.setExtOutputStream shell System/err)
     (.setInputStream shell (java.io.ByteArrayInputStream. (.getBytes "gcc -O2 -g -Wall source.c -o program\nexit\n")))
+    ((get sess :status-observer) "compiling")
     (.connect shell 3000)
-    session
+    ((get sess :status-observer) "compiled")
+    sess
 )
 
-(defn execute-program-on-server [session]
-    (def shell (.openChannel session "shell"))
+(defn execute-program-on-server [sess]
+    ((get sess :status-observer) "initializing execution")
+    (def shell (.openChannel (get sess :session) "shell"))
     (.setOutputStream shell System/out)
     (.setExtOutputStream shell System/err)
     (.setInputStream shell (java.io.ByteArrayInputStream. (.getBytes "./program > output.txt\nexit\n")))
+    ((get sess :status-observer) "executing")
     (.connect shell 3000)
-    session
+    ((get sess :status-observer) "executed")
+    sess
 )
 
 
