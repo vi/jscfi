384ae99b42f598f2687d475193c3c67838386e9a
Vitaly _Vi Shukela <public_vi@tut.by>
2011-04-04 14:42:03 +0300

    Move forward task list parsing

--- org/vi_server/jscfi/real.clj
+++ org/vi_server/jscfi/real.clj
@@ -1,6 +1,7 @@
 (ns org.vi-server.jscfi.real
  "Real Jscfi that interacts with with SSH"
  (:use org.vi-server.jscfi.jscfi)
+ (:use [clojure.contrib.string :only [split join upper-case lower-case trim blank?]])
  (:import (com.jcraft.jsch JSch Channel Session UserInfo UIKeyboardInteractive ChannelSftp))
  (:import (java.io.ByteArrayInputStream))
  )  
@@ -22,12 +23,58 @@
    (str output))
   (catch Exception e (.printStackTrace e) nil)))
 
+(defn interpret-task-list [qstat-output] "Returns vector of hashes - PBS's tasks"
+    (comment #_"
+    Job Id: 89.server-2
+    Job_Name = mpiwc
+    Job_Owner = shukela@server-2
+    resources_used.cput = 00:00:00
+    resources_used.mem = 412kb
+    resources_used.vmem = 8284kb
+    resources_used.walltime = 00:00:02
+    job_state = E
+    queue = batch
+    server = server-2
+    Checkpoint = u
+    ctime = Mon Apr  4 14:29:29 2011
+    Error_Path = server-2:/home/shukela/testscript/mpiwc.e89
+    exec_host = node-099/0+node-098/0
+    Hold_Types = n
+    Join_Path = n
+    Keep_Files = n
+    Mail_Points = a
+    mtime = Mon Apr  4 14:29:31 2011
+    Output_Path = server-2:/home/shukela/testscript/mpiwc.o89
+    Priority = 0
+    qtime = Mon Apr  4 14:29:29 2011
+    Rerunable = True
+    Resource_List.nodect = 2
+    Resource_List.nodes = 2
+    Resource_List.walltime = 00:00:01
+    Variable_List = PBS_O_HOME=/home/shukela,PBS_O_LANG=en_US.UTF-8,
+	PBS_O_LOGNAME=shukela,
+	PBS_O_PATH=/usr/kerberos/bin:/usr/local/bin:/bin:/usr/bin:/usr/local/
+	cnet/bin/:/usr/NX/bin:/usr/libexec/nx:/home/shukela/bin,
+	PBS_O_MAIL=/var/spool/mail/shukela,PBS_O_SHELL=/bin/bash,
+	PBS_SERVER=server-2,PBS_O_HOST=server-2,
+	PBS_O_WORKDIR=/home/shukela/testscript,PBS_O_QUEUE=batch
+    etime = Mon Apr  4 14:29:29 2011
+    exit_status = 0
+    submit_args = run.pbs 
+    ")
+    qstat-output
+)
+
 (deftype RealJscfi [state-agent] Jscfi
     (get-tasks [this] 
      (let [state @state-agent]
       (when (:connected state)
-	(let [session (:session state)]
-	 (println (ssh-execute session "qstat -a | perl -ne 'chomp; next if /Job id\\s+Name/; next if /^-----------/; s/\\s+/:/g; print \"$_\n\"'" nil))))
+	(let [
+	 session (:session state)
+	 tasklist (ssh-execute session "qstat -f" nil)
+	 ]
+	 (println (interpret-task-list tasklist))
+	 ))
       (:tasks @state-agent)
       ))
     (get-task [this id] (loop [t (:tasks @state-agent)] (if (= (:id (first t)) id) (first t) (if (empty? t) nil (recur (next t) )))))
--- start2.sh
+++ start2.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-AWT_TOOLKIT=MToolkit java -cp .:/usr/share/java/clojure.jar:/usr/share/java/jsch.jar:/usr/share/java/miglayout.jar clojure.main org/vi_server/jscfi/main.clj
+AWT_TOOLKIT=MToolkit java -cp .:/usr/share/java/clojure.jar:/usr/share/java/clojure-contrib.jar:/usr/share/java/jsch.jar:/usr/share/java/miglayout.jar clojure.main org/vi_server/jscfi/main.clj
