7bbafbf899ea01d70596ac5569f8ba4f2b7be437
Vitaly _Vi Shukela <public_vi@tut.by>
2011-10-24 14:46:21 +0300

    Jscfi 0.9

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject 
    jscfi
-   "0.8-SNAPSHOT" 
+   "0.9-SNAPSHOT" 
    :description "GUI for uploading MPI programs into PBS-controlled servers" 
    :dependencies [[org.clojure/clojure "1.3.0-alpha4"]
                   [org.clojure/clojure-contrib "1.2.0"]
