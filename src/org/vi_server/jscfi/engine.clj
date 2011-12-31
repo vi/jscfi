521b157c6f73288b8f0d7b9bbc36bb7eea2418a0
Vitaly _Vi Shukela <vi0oss@gmail.com>
2011-12-31 05:52:43 +0300

    Support overriding of scripts directory using JSCFI_SCRIPTS envvar

--- src/org/vi_server/jscfi/engine.clj
+++ src/org/vi_server/jscfi/engine.clj
@@ -11,9 +11,30 @@
  (:import (java.io ByteArrayInputStream File))
  )  
 
-(defn read-script-noformat [script-name] 
+(defn read-script-from-jar [script-name]
  (slurp (ClassLoader/getSystemResourceAsStream 
 	 (str "org/vi_server/jscfi/scripts/" script-name))))
+
+(defn read-script-from-file [scripts-path script-name]
+ (slurp 
+  (-> scripts-path
+   (java.io.File.) 
+   (.toURI) 
+   (str "/" script-name) 
+   (java.net.URI.) 
+   (java.io.File.)
+   (java.io.FileInputStream.))))
+
+(defn read-script-noformat [script-name] 
+ (let
+  [scripts-path (System/getenv "JSCFI_SCRIPTS")]
+  (if (empty? scripts-path)
+   (read-script-from-jar script-name)
+   (try 
+    (read-script-from-file scripts-path script-name)
+    (catch Exception e (println e) 
+     (println "Using embedded version of " script-name)
+     (read-script-from-jar script-name))))))
 
 (defn read-script [script-name & args] 
  (apply format (read-script-noformat script-name) args))
