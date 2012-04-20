444405f353a05945fba209366411c753841a4ee7
Vitaly _Vi Shukela <vi0oss@gmail.com>
2012-04-20 17:37:43 +0300

    Update start2.sh script (to start jscfi without building it)

--- start2.sh
+++ start2.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 JP=/usr/share/java
-DEPS=$JP/clojure.jar:$JP/clojure-contrib.jar:$JP/jsch.jar:$JP/miglayout.jar
+DEPS=$JP/clojure.jar:$JP/tools.logging-0.2.3.jar:$JP/log4j-1.2.15.jar:$JP/jsch.jar:$JP/miglayout.jar
 cd src
-AWT_TOOLKIT=MToolkit java -cp .:$DEPS clojure.main org/vi_server/jscfi/main.clj
+AWT_TOOLKIT=MToolkit java -cp .:$DEPS clojure.main -e '(load-file "org/vi_server/jscfi/main.clj") (org.vi-server.jscfi.main/-main)'
