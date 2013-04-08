ad7dd523b03aa6713fddbccb53196a09fca42f18
Vitaly _Vi Shukela <vi0oss@gmail.com>
2013-04-08 03:00:14 +0300

    Bump version to 1.7

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject 
    jscfi
-   "1.6" 
+   "1.7" 
    :description "GUI for uploading MPI programs into PBS-controlled servers" 
    :dependencies [[org.clojure/clojure "1.3.0-alpha4"] 
                  #_ "Alpha version because of http://dev.clojure.org/jira/browse/CLJ-855"
